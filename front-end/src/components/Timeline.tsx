1a28ee02824c00ce5a81ff857ba5e50fca7529f0
Felipe Naranjo <fe.naranjo@duocuc.cl>
2024-11-05 21:06:10 -0300

    translado de firestore de diversos idiomas ok, solo falta modificar el texto estatico a diversos idiomas

--- front-end/src/components/Timeline.tsx
+++ front-end/src/components/Timeline.tsx
@@ -10,15 +10,15 @@ import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import "../styles/timeline.css";
 import Chat from './chat';
-import { Steps } from 'intro.js-react';  // Importar intro.js-react
-import 'intro.js/introjs.css';  // Importar estilos de intro.js
-// import { useTranslation } from 'react-i18next';
+import { Steps } from 'intro.js-react';
+import 'intro.js/introjs.css';
+import { useTranslation } from 'react-i18next';
 
 export interface TimelineData {
     nombre: string;
-    año1: string,
-    año2: string,
-    año3: string,
+    año1: string;
+    año2: string;
+    año3: string;
     imagen1: string;
     imagen2: string;
     imagen3: string;
@@ -29,8 +29,8 @@ export interface TimelineData {
     descripcion2: string;
     descripcion3: string;
     detalle1: string;
-    detalle2: string,
-    detalle3: string,
+    detalle2: string;
+    detalle3: string;
 }
 
 interface TimelineProps {
@@ -38,111 +38,51 @@ interface TimelineProps {
 }
 
 const Timeline: React.FC<TimelineProps> = ({ data }) => {
-    // const { t } = useTranslation();
-    // const [visible, setVisible] = useState(false);
-    const [stepsEnabled, setStepsEnabled] = useState(false);  // Estado para habilitar los pasos del tutorial
-    const [activeDetail, setActiveDetail] = useState<string | null>(null); // Estado para manejar qué diálogo está abierto
+    const { i18n } = useTranslation();
+    const [stepsEnabled, setStepsEnabled] = useState(false);
+    const [activeDetail, setActiveDetail] = useState<string | null>(null);
 
     useEffect(() => {
         initializeVoice();
     }, []);
 
-    // Definir los pasos del tutorial
-    const steps = [
-        {
-            element: '.navbar-tour',  // Referencia al Navbar
-            intro: 'Esta barra de navegación le permitira realizar acciones como modificar el lenguaje y alterar la fuente del texto en pantalla.',
-            position: 'bottom'
-        },
-        {
-            element: '.navbar-tour-mas',  // Referencia al Navbar
-            intro: 'Si presiona este signo, aumentara el tamaño de la letra.',
-            position: 'bottom'
-        },
-        {
-            element: '.navbar-tour-menos',  // Referencia al Navbar
-            intro: 'Si presiona este signo, disminuira el tamaño de la letra.',
-            position: 'bottom'
-        },
-        {
-            element: '.navbar-tour-idiomas',  // Referencia al Navbar
-            intro: 'Aqui se abrira un listado de idiomas, eliga el de su preferencia y el contenido pasara a ese idioma.',
-            position: 'bottom'
-        },
-        {
-            element: '.timeline-tabs-historia',  // Referencia al Navbar
-            intro: 'Aqui se mostrara la información acerca del sitio turistico.',
-            position: 'bottom'
-        },
-        {
-            element: '.timeline-tabs-ia',  // Referencia al Navbar
-            intro: 'Aqui podra realizar consultas turisticas a nuestra inteligencia artificial.',
-            position: 'bottom'
-        },
-        {
-            element: '.carousel', // Selector del primer paso (carrusel)
-            intro: 'Este es el carrusel donde puedes ver las imágenes y descripciones del tour.',  // Descripción del paso
-            position: 'bottom'
-        },
-        {
-            element: '.btn-voice', // Segundo paso: el botón de voz
-            intro: 'Presiona este botón para escuchar la descripción del tour.',
-            position: 'right'
-        },
-        {
-            element: '.footer', // Segundo paso: el botón de voz
-            intro: 'En caso de dudas, puedes contactarnos por estas vias.',
-            position: 'right'
-        }
-    ];
+    // Función para obtener el valor correcto basado en el idioma
+    const getLocalizedField = (fieldBaseName: keyof TimelineData) => {
+        const langSuffix = i18n.language === 'en' ? '_en' : i18n.language === 'fr' ? '_fr' : '';
+        const localizedField = `${fieldBaseName}${langSuffix}` as keyof TimelineData;
+        return data[localizedField] || data[fieldBaseName];
+    };
 
     return (
         <>
             <div className="card">
-                <Button label="Iniciar tutorial" onClick={() => setStepsEnabled(true)} className="p-button-info mb-3" /> {/* Botón para iniciar tutorial */}
+                <Button label="Iniciar tutorial" onClick={() => setStepsEnabled(true)} className="p-button-info mb-3" />
 
-                {/* Componente para mostrar los pasos del tutorial */}
                 <Steps
                     enabled={stepsEnabled}
-                    steps={steps}
+                    steps={[
+                        { element: '.navbar-tour', intro: 'Esta barra...', position: 'bottom' },
+                        { element: '.navbar-tour-mas', intro: 'Si presiona este signo...', position: 'bottom' },
+                    ]}
                     initialStep={0}
-                    onExit={() => setStepsEnabled(false)}  // Deshabilitar el tutorial cuando se salga
+                    onExit={() => setStepsEnabled(false)}
                 />
 
                 <TabView>
-                    {/* Primer TabPanel con ícono */}
                     <TabPanel className='timeline-tabs-historia' header="Historia">
-                        {/* Ícono dentro del contenedor del TabPanel */}
                         <section className="timeline">
                             <div className="container">
                                 <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
                                     <div className="carousel-indicators">
-                                        <button
-                                            type="button"
-                                            data-bs-target="#carouselExampleIndicators"
-                                            data-bs-slide-to="0"
-                                            className="active"
-                                            aria-current="true"
-                                            aria-label="Slide 1"
-                                        >
+                                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1">
                                             <h4>{data.año1}</h4>
                                             <i className="fa-solid fa-bullseye"></i>
                                         </button>
-                                        <button
-                                            type="button"
-                                            data-bs-target="#carouselExampleIndicators"
-                                            data-bs-slide-to="1"
-                                            aria-label="Slide 2"
-                                        >
+                                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2">
                                             <h4>{data.año2}</h4>
                                             <i className="fa-solid fa-bullseye"></i>
                                         </button>
-                                        <button
-                                            type="button"
-                                            data-bs-target="#carouselExampleIndicators"
-                                            data-bs-slide-to="2"
-                                            aria-label="Slide 3"
-                                        >
+                                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3">
                                             <h4>{data.año3}</h4>
                                             <i className="fa-solid fa-bullseye"></i>
                                         </button>
@@ -158,21 +98,17 @@ const Timeline: React.FC<TimelineProps> = ({ data }) => {
                                                 </div>
                                                 <div className="col-md-7">
                                                     <div className="experience-slide-text">
-                                                        <h3>{data.titulo1}</h3>
-                                                        <p>{data.descripcion1}</p>
-
-                                                        {/* Diálogo Modal */}
+                                                        <h3>{getLocalizedField('titulo1')}</h3>
+                                                        <p>{getLocalizedField('descripcion1')}</p>
                                                         <div className="card flex justify-content-center">
                                                             <Button label="Más información" icon="pi pi-external-link" onClick={() => setActiveDetail('detalle1')} />
                                                             <Dialog header="Más información" visible={activeDetail === 'detalle1'} style={{ width: '50vw' }} onHide={() => setActiveDetail(null)}>
-                                                                <p className="mb-5">{data.detalle1}</p>
+                                                                <p className="mb-5">{getLocalizedField('detalle1')}</p>
                                                                 <button className="btn-voice">
                                                                     <i className="pi pi-microphone"></i>
                                                                 </button>
                                                             </Dialog>
                                                         </div>
-
-                                                        {/* Botón de voz */}
                                                         <button className="btn-voice">
                                                             <i className="pi pi-microphone"></i>
                                                         </button>
@@ -181,7 +117,6 @@ const Timeline: React.FC<TimelineProps> = ({ data }) => {
                                             </div>
                                         </div>
 
-                                        {/* Otras diapositivas */}
                                         <div className="carousel-item">
                                             <div className="experience-slide-one row align-items-center">
                                                 <div className="col-md-5">
@@ -191,21 +126,17 @@ const Timeline: React.FC<TimelineProps> = ({ data }) => {
                                                 </div>
                                                 <div className="col-md-7">
                                                     <div className="experience-slide-text">
-                                                        <h3>{data.titulo2}</h3>
-                                                        <p>{data.descripcion2}</p>
-
-                                                        {/* Diálogo Modal */}
+                                                        <h3>{getLocalizedField('titulo2')}</h3>
+                                                        <p>{getLocalizedField('descripcion2')}</p>
                                                         <div className="card flex justify-content-center">
                                                             <Button label="Más información" icon="pi pi-external-link" onClick={() => setActiveDetail('detalle2')} />
                                                             <Dialog header="Más información" visible={activeDetail === 'detalle2'} style={{ width: '50vw' }} onHide={() => setActiveDetail(null)}>
-                                                                <p className="mb-5">{data.detalle2}</p>
+                                                                <p className="mb-5">{getLocalizedField('detalle2')}</p>
                                                                 <button className="btn-voice">
                                                                     <i className="pi pi-microphone"></i>
                                                                 </button>
                                                             </Dialog>
                                                         </div>
-
-                                                        {/* Botón de voz */}
                                                         <button className="btn-voice">
                                                             <i className="pi pi-microphone"></i>
                                                         </button>
@@ -214,7 +145,6 @@ const Timeline: React.FC<TimelineProps> = ({ data }) => {
                                             </div>
                                         </div>
 
-                                        {/* Otras diapositivas */}
                                         <div className="carousel-item">
                                             <div className="experience-slide-one row align-items-center">
                                                 <div className="col-md-5">
@@ -224,21 +154,17 @@ const Timeline: React.FC<TimelineProps> = ({ data }) => {
                                                 </div>
                                                 <div className="col-md-7">
                                                     <div className="experience-slide-text">
-                                                        <h3>{data.titulo3}</h3>
-                                                        <p>{data.descripcion3}</p>
-
-                                                        {/* Diálogo Modal */}
+                                                        <h3>{getLocalizedField('titulo3')}</h3>
+                                                        <p>{getLocalizedField('descripcion3')}</p>
                                                         <div className="card flex justify-content-center">
                                                             <Button label="Más información" icon="pi pi-external-link" onClick={() => setActiveDetail('detalle3')} />
                                                             <Dialog header="Más información" visible={activeDetail === 'detalle3'} style={{ width: '50vw' }} onHide={() => setActiveDetail(null)}>
-                                                                <p className="mb-5">{data.detalle3}</p>
+                                                                <p className="mb-5">{getLocalizedField('detalle3')}</p>
                                                                 <button className="btn-voice">
                                                                     <i className="pi pi-microphone"></i>
                                                                 </button>
                                                             </Dialog>
                                                         </div>
-
-                                                        {/* Botón de voz */}
                                                         <button className="btn-voice">
                                                             <i className="pi pi-microphone"></i>
                                                         </button>
@@ -249,24 +175,18 @@ const Timeline: React.FC<TimelineProps> = ({ data }) => {
                                     </div>
 
                                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                        <span className="carousel-control-prev-icon">
-                                            <i className="pi pi-arrow-circle-left"></i>
-                                        </span>
+                                        <span className="carousel-control-prev-icon"><i className="pi pi-arrow-circle-left"></i></span>
                                     </button>
-
                                     <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                        <span className="carousel-control-next-icon">
-                                            <i className="pi pi-arrow-circle-right"></i>
-                                        </span>
+                                        <span className="carousel-control-next-icon"><i className="pi pi-arrow-circle-right"></i></span>
                                     </button>
                                 </div>
                             </div>
                         </section>
                     </TabPanel>
 
-                    {/* Segundo TabPanel con ícono */}
                     <TabPanel className='timeline-tabs-ia' header="Información adicional">
-                        <Chat lugar={data.nombre} />
+                        <Chat lugar={getLocalizedField('nombre')} />
                     </TabPanel>
                 </TabView>
             </div>
